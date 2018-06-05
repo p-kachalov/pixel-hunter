44a717a7d45639450a000552bd9578d5fee7ca97
Pavel K <p.kachalov@yandex.ru>
2018-06-05 16:38:14 +0300

    code style

--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,19 @@
 
 export default class Timer {
   constructor(time, callback) {
-    this.currentTime = time >= 0 ? time : 0;
+    this._currentTime = time >= 0 ? time : 0;
     this.callback = callback;
   }
 
   get time() {
-    return this.currentTime;
+    return this._currentTime;
   }
 
   tick() {
-    if (this.currentTime > 0) {
-      this.currentTime -= 1;
+    if (this._currentTime > 0) {
+      this._currentTime -= 1;
     }
-    if (this.currentTime === 0) {
+    if (this._currentTime === 0) {
       this.callback();
     }
   }
