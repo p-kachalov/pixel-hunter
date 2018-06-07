7f16d71405a3dd597b013b0cd5095fac01cb64f8
Pavel K <p.kachalov@yandex.ru>
2018-06-08 00:04:29 +0300

    reduce stats calc function

--- js/stats-calc.js
+++ js/stats-calc.js
@@ -1,13 +1,7 @@
 
 export default (answers, lives) => {
-  if (answers.length < 10) {
-    return -1;
-  }
-
-  return answers.reduce((acc, item) => {
-    if (!item.right) {
-      return acc;
-    }
-    return acc + 100 + (item.fast ? 50 : 0) - (item.slow ? 50 : 0);
-  }, 0) + lives * 50;
+  return answers.length < 10 ? -1 :
+    answers.reduce((acc, item) => {
+      return item.right ? acc + 100 + (item.fast ? 50 : 0) - (item.slow ? 50 : 0) : acc;
+    }, 0) + lives * 50;
 };
