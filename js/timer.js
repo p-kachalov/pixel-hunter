34ea17a63689e9c623ada92ce4a114adfad3b2c4
Pavel K <p.kachalov@yandex.ru>
2018-06-07 13:40:32 +0300

    remake timer

--- js/timer.js
+++ js/timer.js
@@ -1,20 +1,13 @@
 
-export default class Timer {
-  constructor(time, callback) {
-    this._currentTime = time >= 0 ? time : 0;
-    this.callback = callback;
-  }
-
-  get time() {
-    return this._currentTime;
-  }
-
-  tick() {
-    if (this._currentTime > 0) {
-      this._currentTime -= 1;
+export default (setTime) => {
+  const timer = {
+    time: setTime > 0 ? setTime : 0,
+    tick: () => {
+      if (timer.time > 0) {
+        timer.time -= 1;
+      }
     }
-    if (this._currentTime === 0) {
-      this.callback();
-    }
-  }
-}
+  };
+
+  return timer;
+};
--- js/timer.test.js
+++ js/timer.test.js
@@ -1,22 +1,22 @@
 import {assert} from 'chai';
-import Timer from './timer';
+import createTimer from './timer';
 
 
 describe(`Timer`, () => {
   describe(`return timer object which`, () => {
     it(`should be an object`, () => {
-      const timer = new Timer(0, () => {});
+      const timer = createTimer(0);
       assert.isObject(timer);
     });
 
     it(`should has time property`, () => {
-      const timer = new Timer(5, () => {});
+      const timer = createTimer(5);
       assert.exists(timer.time);
       assert.isNumber(timer.time);
     });
 
     it(`should has tick function`, () => {
-      const timer = new Timer(5, () => {});
+      const timer = createTimer(5);
       assert.exists(timer.tick);
       assert.isFunction(timer.tick);
     });
@@ -24,48 +24,38 @@ describe(`Timer`, () => {
 
   describe(`time property`, () => {
     it(`should content correct value`, () => {
-      const timer = new Timer(5, () => {});
+      const timer = createTimer(5);
       assert.equal(timer.time, 5);
     });
 
     it(`should not be negative`, () => {
-      const timer = new Timer(-5, () => {});
+      const timer = createTimer(-5);
       assert.equal(timer.time, 0);
     });
   });
 
   describe(`tick function`, () => {
     it(`should decrease time`, () => {
-      const timer = new Timer(5, () => {});
+      const timer = createTimer(5);
       timer.tick();
       timer.tick();
       assert.equal(timer.time, 3);
     });
 
     it(`should not decrease time less than 0`, () => {
-      const timer = new Timer(2, () => {});
+      const timer = createTimer(2);
       timer.tick();
       timer.tick();
       timer.tick();
       timer.tick();
       assert.equal(timer.time, 0);
     });
-
-    it(`should call back when timer is over`, () => {
-      let over = false;
-      const timer = new Timer(2, () => {
-        over = true;
-      });
-      timer.tick();
-      timer.tick();
-      assert.isTrue(over);
-    });
   });
 
   describe(`and multiple timers`, () => {
     it(`should be unrelated`, () => {
-      const timer1 = new Timer(5, () => {});
-      const timer2 = new Timer(5, () => {});
+      const timer1 = createTimer(5);
+      const timer2 = createTimer(5);
       timer1.tick();
       timer1.tick();
       assert.equal(timer1.time, 3);
