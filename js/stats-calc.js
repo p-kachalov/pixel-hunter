302db4c41b3ee702956b121832f9c5c9ba62b444
Pavel K <p.kachalov@yandex.ru>
2018-06-08 08:10:05 +0300

    stats calc readability

--- js/stats-calc.js
+++ js/stats-calc.js
@@ -1,5 +1,23 @@
 
-export default (answers, lives) => answers.length < 10 ? -1 :
-  answers.reduce((acc, item) => item.right ?
-    acc + 100 + (item.fast ? 50 : 0) - (item.slow ? 50 : 0) :
-    acc, 0) + lives * 50;
+const LIVE_BONUS = 50;
+const SPEED_BONUS = 50;
+const SPEED_PENALTY = 50;
+const RIGHT_ANSWER_SCORE = 100;
+const ANSWERS_COUNT = 10;
+
+export default (answers, lives) => {
+  if (answers.length < ANSWERS_COUNT) {
+    return -1;
+  }
+
+  return answers.reduce((acc, item) => {
+    if (!item.right) {
+      return acc;
+    }
+
+    const bonus = item.fast ? SPEED_BONUS : 0;
+    const penalty = item.slow ? SPEED_PENALTY : 0;
+
+    return acc + RIGHT_ANSWER_SCORE + bonus - penalty;
+  }, lives * LIVE_BONUS);
+};
