1c1072d463d333bc7db6e328385c4376d35317ed
Pavel K <p.kachalov@yandex.ru>
2018-06-13 10:22:27 +0300

    render game stats on game screen

--- js/game-stats.js
+++ js/game-stats.js
@@ -1,25 +1,49 @@
 import renderTemplate from './render-template';
+import AnswerSpeed from './answer-speed';
 
 const statsTemplate = `
 <div class="stats">
   <ul class="stats">
-    <li class="stats__result stats__result--wrong"></li>
-    <li class="stats__result stats__result--slow"></li>
-    <li class="stats__result stats__result--fast"></li>
-    <li class="stats__result stats__result--correct"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
   </ul>
 </div>
 `;
 
-export default () => {
+const wrongTemplate = `<li class="stats__result stats__result--wrong"></li>`;
+const slowTemplate = `<li class="stats__result stats__result--slow"></li>`;
+const fastTemplate = `<li class="stats__result stats__result--fast"></li>`;
+const correctTemplate = `<li class="stats__result stats__result--correct"></li>`;
+const unknownTemplate = `<li class="stats__result stats__result--unknown"></li>`;
+
+const chooseAnswerTemplate = (answer) => {
+  if (!answer) {
+    return unknownTemplate;
+  }
+
+  if (!answer.right) {
+    return wrongTemplate;
+  }
+
+  if (answer.speed === AnswerSpeed.FAST) {
+    return fastTemplate;
+  }
+
+  if (answer.speed === AnswerSpeed.SLOW) {
+    return slowTemplate;
+  }
+
+  return correctTemplate;
+};
+
+export default (state) => {
   const screen = document.createElement(`template`);
   const stats = renderTemplate(statsTemplate);
+
+  const answers = stats.querySelector(`ul.stats`);
+
+  state.answers.forEach((answer) => {
+    const answerTemplate = chooseAnswerTemplate(answer);
+    answers.appendChild(renderTemplate(answerTemplate));
+  });
 
   screen.content.appendChild(stats);
 
