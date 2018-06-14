86cbabdefb02be4e4fe69580b6ee8b897f02f992
Pavel K <p.kachalov@yandex.ru>
2018-06-14 09:28:10 +0300

    rename game screen file

--- js/render-screen.js
+++ js/render-screen.js
@@ -1,7 +1,7 @@
 import getIntro from './screen-intro';
-import getGreeting from './screen-greeting';
+import getGreeting from '../screen-greeting';
 import getRuls from './screen-rules';
-import getGame from './game-screen';
+import getGame from './screen-game';
 import getStats from './screen-stats';
 
 const container = document.querySelector(`.central`);
