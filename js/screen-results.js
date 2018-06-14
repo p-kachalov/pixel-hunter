f456e650e0082c0a59a576fc60ddd96a6cf3dc2b
Pavel K <p.kachalov@yandex.ru>
2018-06-14 22:28:22 +0300

    code style

--- js/screen-results.js
+++ js/screen-results.js
@@ -1,12 +1,8 @@
 import renderTemplate from './render-template';
 import getFooter from './block-footer';
 import getHeader from './block-header';
-
-const contentTemplate = `
-<div class="result">
-  <h1>Победа!</h1>
-</div>
-`;
+import getStats from './block-stats';
+import AnswerSpeed from './answer-speed';
 
 const user1Template = `
 <table class="result__table">
@@ -114,14 +110,85 @@ const user3Template = `
 </table>
 `;
 
+const contentTemplate = `
+<div class="result">
+  <h1>Победа!</h1>
+</div>
+`;
+
+const getResultTemplate = (result) => {
+  const totalFailTemplate = `
+  <td class="result__total"></td>
+  <td class="result__total  result__total--final">fail</td>
+  `;
+
+  const totalPointsTemplate = `
+  <td class="result__points">×&nbsp;${result.points}</td>
+  <td class="result__total">${result.totalPoints}</td>
+  `;
+
+  const totalTemplate = result.fail ? totalFailTemplate : totalPointsTemplate;
+
+  const resultTemplate = `
+  <table class="result__table">
+    <tr>
+      <td class="result__number">${result.number}.</td>
+      <td colspan="2">${result.stats}</td>
+      ${totalTemplate}
+    </tr>
+  </table>
+  `;
+
+  return resultTemplate;
+};
+
+const processResultsData = (data) => {
+  const results = data.map((item, index) => {
+    const fail = item.lives === 0;
+    const stats = renderTemplate(getStats(item.answers));
+    const points = 100; // constant
+    const rightAnswer = item.answers.filter((answer) => answer.right).length;
+    const totalPoints = points * rightAnswer;
+
+    return {number: index, fail, stats, points, totalPoints};
+  });
+  return results;
+};
+
+const data = [
+  {
+    lives: 3,
+    answers: [
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+    ],
+  }
+];
+
+
 export default (state, callback) => {
   const screen = document.createElement(`template`);
   const header = getHeader(state, callback);
   const footer = getFooter();
-
-
-
   const content = renderTemplate(contentTemplate);
+
+
+  const resultsData = processResultsData(data);
+
+  resultsData.forEach((item) => {
+    const resultTemplate = getResultTemplate(item);
+    const result = renderTemplate(resultTemplate);
+    content.appendChild(result);
+  });
+
 
   const user1 = renderTemplate(user1Template);
   const user2 = renderTemplate(user2Template);
@@ -129,7 +196,6 @@ export default (state, callback) => {
   content.appendChild(user1);
   content.appendChild(user2);
   content.appendChild(user3);
-
 
 
   screen.content.appendChild(header);
