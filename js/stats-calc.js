512a363278e4e8ea7af3cef38a1d7e00dcc7f7e3
Pavel K <p.kachalov@yandex.ru>
2018-06-15 11:31:35 +0300

    add settings

--- js/stats-calc.js
+++ js/stats-calc.js
@@ -1,25 +1,22 @@
 
 import AnswerSpeed from './answer-speed';
 
-const LIVE_BONUS = 50;
-const RIGHT_ANSWER_SCORE = 100;
-const ANSWERS_COUNT = 10;
-const SPEED_RATE = {
-  [AnswerSpeed.NORMAL]: 0,
-  [AnswerSpeed.FAST]: 50,
-  [AnswerSpeed.SLOW]: -50,
-};
-
-export default (answers, lives) => {
-  if (answers.length < ANSWERS_COUNT) {
+export default (answers, lives, settings) => {
+  if (answers.length < settings.questionNumber) {
     return -1;
   }
+
+  const SPEED_RATE = {
+    [AnswerSpeed.NORMAL]: 0,
+    [AnswerSpeed.FAST]: settings.fastCost,
+    [AnswerSpeed.SLOW]: -(settings.slowCost),
+  };
 
   return answers.reduce((acc, item) => {
     if (!item.right) {
       return acc;
     }
 
-    return acc + RIGHT_ANSWER_SCORE + SPEED_RATE[item.speed];
-  }, lives * LIVE_BONUS);
+    return acc + settings.answerCost + SPEED_RATE[item.speed];
+  }, lives * settings.liveCost);
 };
--- js/stats-calc.test.js
+++ js/stats-calc.test.js
@@ -10,18 +10,26 @@ const replicateObject = (obj, count) => {
   return result;
 };
 
+const settings = {
+  questionNumber: 10,
+  answerCost: 100,
+  fastCost: 50,
+  slowCost: 50,
+  liveCost: 50,
+};
+
 describe(`statistics`, () => {
   describe(`with valid input`, () => {
     it(`should return -1 when user lost`, () => {
       const answers = replicateObject({right: false, speed: AnswerSpeed.NORMAL}, 4);
       const lives = 0;
-      assert.equal(-1, statsCalc(answers, lives));
+      assert.equal(-1, statsCalc(answers, lives, settings));
     });
 
     it(`should return 1150 when user won with 3 live, without penalties and bonuses`, () => {
       const answers = replicateObject({right: true, speed: AnswerSpeed.NORMAL}, 10);
       const lives = 3;
-      assert.equal(1150, statsCalc(answers, lives));
+      assert.equal(1150, statsCalc(answers, lives, settings));
     });
 
     it(`should return 1200 when user won with 3 live, 2 penalties and 3 bonuses`, () => {
@@ -29,7 +37,7 @@ describe(`statistics`, () => {
         .concat(replicateObject({right: true, speed: AnswerSpeed.FAST}, 3))
         .concat(replicateObject({right: true, speed: AnswerSpeed.SLOW}, 2));
       const lives = 3;
-      assert.equal(1200, statsCalc(answers, lives));
+      assert.equal(1200, statsCalc(answers, lives, settings));
     });
 
     it(`should return 1000 when user won with 1 live, 3 penalties and 2 bonuses`, () => {
@@ -37,7 +45,7 @@ describe(`statistics`, () => {
         .concat(replicateObject({right: true, speed: AnswerSpeed.FAST}, 2))
         .concat(replicateObject({right: true, speed: AnswerSpeed.SLOW}, 3));
       const lives = 1;
-      assert.equal(1000, statsCalc(answers, lives));
+      assert.equal(1000, statsCalc(answers, lives, settings));
     });
   });
 });
