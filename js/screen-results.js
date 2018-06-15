4e842914e77f7240e394bfa8a860555d0df2253f
Pavel K <p.kachalov@yandex.ru>
2018-06-15 11:42:12 +0300

    render results screen from data

--- js/screen-results.js
+++ js/screen-results.js
@@ -2,119 +2,8 @@ import renderTemplate from './render-template';
 import getFooter from './block-footer';
 import getHeader from './block-header';
 import getStats from './block-stats';
+import statsCalc from './stats-calc';
 import AnswerSpeed from './answer-speed';
-
-const user1Template = `
-<table class="result__table">
-  <tr>
-    <td class="result__number">1.</td>
-    <td colspan="2">
-      <ul class="stats">
-        <li class="stats__result stats__result--wrong"></li>
-        <li class="stats__result stats__result--slow"></li>
-        <li class="stats__result stats__result--fast"></li>
-        <li class="stats__result stats__result--correct"></li>
-        <li class="stats__result stats__result--wrong"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--slow"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--fast"></li>
-        <li class="stats__result stats__result--unknown"></li>
-      </ul>
-    </td>
-    <td class="result__points">×&nbsp;100</td>
-    <td class="result__total">900</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td class="result__extra">Бонус за скорость:</td>
-    <td class="result__extra">1&nbsp;<span class="stats__result stats__result--fast"></span></td>
-    <td class="result__points">×&nbsp;50</td>
-    <td class="result__total">50</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td class="result__extra">Бонус за жизни:</td>
-    <td class="result__extra">2&nbsp;<span class="stats__result stats__result--alive"></span></td>
-    <td class="result__points">×&nbsp;50</td>
-    <td class="result__total">100</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td class="result__extra">Штраф за медлительность:</td>
-    <td class="result__extra">2&nbsp;<span class="stats__result stats__result--slow"></span></td>
-    <td class="result__points">×&nbsp;50</td>
-    <td class="result__total">-100</td>
-  </tr>
-  <tr>
-    <td colspan="5" class="result__total  result__total--final">950</td>
-  </tr>
-</table>
-`;
-
-const user2Template = `
-<table class="result__table">
-  <tr>
-    <td class="result__number">2.</td>
-    <td>
-      <ul class="stats">
-        <li class="stats__result stats__result--wrong"></li>
-        <li class="stats__result stats__result--slow"></li>
-        <li class="stats__result stats__result--fast"></li>
-        <li class="stats__result stats__result--correct"></li>
-        <li class="stats__result stats__result--wrong"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--slow"></li>
-        <li class="stats__result stats__result--wrong"></li>
-        <li class="stats__result stats__result--fast"></li>
-        <li class="stats__result stats__result--wrong"></li>
-      </ul>
-    </td>
-    <td class="result__total"></td>
-    <td class="result__total  result__total--final">fail</td>
-  </tr>
-</table>
-`;
-
-const user3Template = `
-<table class="result__table">
-  <tr>
-    <td class="result__number">3.</td>
-    <td colspan="2">
-      <ul class="stats">
-        <li class="stats__result stats__result--wrong"></li>
-        <li class="stats__result stats__result--slow"></li>
-        <li class="stats__result stats__result--fast"></li>
-        <li class="stats__result stats__result--correct"></li>
-        <li class="stats__result stats__result--wrong"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--slow"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--fast"></li>
-        <li class="stats__result stats__result--unknown"></li>
-      </ul>
-    </td>
-    <td class="result__points">×&nbsp;100</td>
-    <td class="result__total">900</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td class="result__extra">Бонус за жизни:</td>
-    <td class="result__extra">2&nbsp;<span class="stats__result stats__result--alive"></span></td>
-    <td class="result__points">×&nbsp;50</td>
-    <td class="result__total">100</td>
-  </tr>
-  <tr>
-    <td colspan="5" class="result__total  result__total--final">950</td>
-  </tr>
-</table>
-`;
-
-const contentTemplate = `
-<div class="result">
-  <h1>Победа!</h1>
-</div>
-`;
 
 const getResultTemplate = (result) => {
   const totalFailTemplate = `
@@ -127,14 +16,58 @@ const getResultTemplate = (result) => {
   <td class="result__total">${result.totalPoints}</td>
   `;
 
-  const totalTemplate = result.fail ? totalFailTemplate : totalPointsTemplate;
+  const speetBonusTemplate = `
+  <tr>
+    <td></td>
+    <td class="result__extra">Бонус за скорость:</td>
+    <td class="result__extra">${result.fast}&nbsp;<span class="stats__result stats__result--fast"></span></td>
+    <td class="result__points">×&nbsp;${result.fastCost}</td>
+    <td class="result__total">${result.fast * result.fastCost}</td>
+  </tr>
+  `;
+
+  const livesBonusTemplate = `
+  <tr>
+    <td></td>
+    <td class="result__extra">Бонус за жизни:</td>
+    <td class="result__extra">${result.lives}&nbsp;<span class="stats__result stats__result--alive"></span></td>
+    <td class="result__points">×&nbsp;${result.liveCost}</td>
+    <td class="result__total">${result.lives * result.liveCost}</td>
+  </tr>
+  `;
+
+  const slowPenaltyTemplate = `
+  <tr>
+    <td></td>
+    <td class="result__extra">Штраф за медлительность:</td>
+    <td class="result__extra">${result.slow}&nbsp;<span class="stats__result stats__result--slow"></span></td>
+    <td class="result__points">×&nbsp;${result.slowCost}</td>
+    <td class="result__total">-${result.slow * result.slowCost}</td>
+  </tr>
+  `;
+
+  const totalFinalTemplate = `
+  <tr>
+    <td colspan="5" class="result__total  result__total--final">${result.totalFilnal}</td>
+  </tr>
+  `;
+
+  const totalBlock = result.fail ? totalFailTemplate : totalPointsTemplate;
+  const speedBonusBlock = (!result.fail && result.fast > 0) ? speetBonusTemplate : ``;
+  const livesBonusBlock = (!result.fail && result.lives > 0) ? livesBonusTemplate : ``;
+  const slowPenaltyBlock = (!result.fail && result.slow > 0) ? slowPenaltyTemplate : ``;
+  const totalFinalBlock = (!result.fail && result.totalFilnal > 0) ? totalFinalTemplate : ``;
 
   const resultTemplate = `
   <table class="result__table">
     <tr>
-      <td class="result__number">${result.number}.</td>
-      <td colspan="2">${result.stats}</td>
-      ${totalTemplate}
+      <td class="result__number">${result.gameNumber}.</td>
+      <td colspan="2" class="stats-container"></td>
+      ${totalBlock}
+      ${speedBonusBlock}
+      ${livesBonusBlock}
+      ${slowPenaltyBlock}
+      ${totalFinalBlock}
     </tr>
   </table>
   `;
@@ -143,33 +76,110 @@ const getResultTemplate = (result) => {
 };
 
 const processResultsData = (data) => {
-  const results = data.map((item, index) => {
-    const fail = item.lives === 0;
-    const stats = renderTemplate(getStats(item.answers));
-    const points = 100; // constant
-    const rightAnswer = item.answers.filter((answer) => answer.right).length;
+  const results = data.map((result, index) => {
+    const gameNumber = index + 1;
+    const lives = result.lives;
+    const fail = lives === 0;
+    const stats = getStats(result.answers);
+    const points = result.settings.answerCost;
+    const rightAnswer = result.answers.filter((answer) => answer.right).length;
     const totalPoints = points * rightAnswer;
+    const fast = result.answers.filter((answer) =>
+      answer.right && answer.speed === AnswerSpeed.FAST).length;
+    const slow = result.answers.filter((answer) =>
+      answer.right && answer.speed === AnswerSpeed.SLOW).length;
+    const fastCost = result.settings.fastCost;
+    const liveCost = result.settings.liveCost;
+    const slowCost = result.settings.slowCost;
+    const totalFilnal = statsCalc(result.answers, result.lives, result.settings);
 
-    return {number: index, fail, stats, points, totalPoints};
+    return {
+      gameNumber,
+      fail,
+      stats,
+      points,
+      totalPoints,
+      fast,
+      fastCost,
+      lives,
+      liveCost,
+      slow,
+      slowCost,
+      totalFilnal
+    };
   });
   return results;
 };
 
+const renderResult = (data) => {
+  const resultTemplate = getResultTemplate(data);
+  const result = renderTemplate(resultTemplate);
+  const statsContainer = result.querySelector(`.stats-container`);
+  statsContainer.appendChild(data.stats);
+
+  return result;
+};
+
 const data = [
+  {
+    lives: 2,
+    answers: [
+      {right: false, speed: null},
+      {right: true, speed: AnswerSpeed.SLOW},
+      {right: true, speed: AnswerSpeed.FAST},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.SLOW},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.FAST},
+      {right: true, speed: AnswerSpeed.NORMAL},
+    ],
+    settings: {
+      answerCost: 100,
+      fastCost: 50,
+      slowCost: 50,
+      liveCost: 50,
+    }
+  },
+  {
+    lives: 0,
+    answers: [
+      {right: false, speed: null},
+      {right: true, speed: AnswerSpeed.SLOW},
+      {right: true, speed: AnswerSpeed.FAST},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: false, speed: null},
+      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: false, speed: null},
+    ],
+    settings: {
+      answerCost: 100,
+      fastCost: 50,
+      slowCost: 50,
+      liveCost: 50,
+    }
+  },
   {
     lives: 3,
     answers: [
+      {right: true, speed: AnswerSpeed.SLOW},
+      {right: true, speed: AnswerSpeed.SLOW},
+      {right: true, speed: AnswerSpeed.FAST},
       {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.SLOW},
       {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.SLOW},
       {right: true, speed: AnswerSpeed.NORMAL},
-      {right: true, speed: AnswerSpeed.NORMAL},
-      {right: true, speed: AnswerSpeed.NORMAL},
-      {right: true, speed: AnswerSpeed.NORMAL},
-      {right: true, speed: AnswerSpeed.NORMAL},
-      {right: true, speed: AnswerSpeed.NORMAL},
-      {right: true, speed: AnswerSpeed.NORMAL},
+      {right: true, speed: AnswerSpeed.SLOW},
       {right: true, speed: AnswerSpeed.NORMAL},
     ],
+    settings: {
+      answerCost: 100,
+      fastCost: 50,
+      slowCost: 50,
+      liveCost: 50,
+    }
   }
 ];
 
@@ -178,25 +188,14 @@ export default (state, callback) => {
   const screen = document.createElement(`template`);
   const header = getHeader(state, callback);
   const footer = getFooter();
-  const content = renderTemplate(contentTemplate);
-
+  const content = renderTemplate(`<div class="result"><h1>Победа!</h1></div>`);
 
   const resultsData = processResultsData(data);
 
-  resultsData.forEach((item) => {
-    const resultTemplate = getResultTemplate(item);
-    const result = renderTemplate(resultTemplate);
+  resultsData.forEach((dataItem) => {
+    const result = renderResult(dataItem);
     content.appendChild(result);
   });
-
-
-  const user1 = renderTemplate(user1Template);
-  const user2 = renderTemplate(user2Template);
-  const user3 = renderTemplate(user3Template);
-  content.appendChild(user1);
-  content.appendChild(user2);
-  content.appendChild(user3);
-
 
   screen.content.appendChild(header);
   screen.content.appendChild(content);
