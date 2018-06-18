3efb4f9ff3f6c0d4f894c88c8f56ad22b0c8b796
Pavel K <p.kachalov@yandex.ru>
2018-06-18 18:19:06 +0300

    refactoring game screen

--- js/screen-game.js
+++ js/screen-game.js
@@ -1,3 +1,5 @@
+import Answer from './answer';
+import GameType from './game-type';
 import getFooter from './block-footer';
 import getHeader from './block-header';
 import getStatus from './block-status';
@@ -6,12 +8,6 @@ import gameSingle from './block-game-single';
 import gameDouble from './block-game-double';
 import gameTriple from './block-game-triple';
 import renderTemplate from './render-template';
-import Answer from './answer';
-import GameType from './game-type';
-
-const calcLives = (answer, lives) => {
-  return (answer === Answer.WRONG) ? lives - 1 : lives;
-};
 
 const Game = {
   [GameType.SINGLE]: gameSingle,
@@ -19,33 +15,50 @@ const Game = {
   [GameType.TRIPLE]: gameTriple,
 };
 
-export default (state, callback) => {
-  const screen = document.createElement(`template`);
-  const header = getHeader(state, callback);
-  const status = getStatus(state);
-  const footer = getFooter();
+const getGameTemplate = (questionText) => {
+  return `
+  <div class="game">
+    <p class="game__task">${questionText}</p>
+  </div>
+  `;
+};
 
+const getGame = (state, callback) => {
+  // render game content
   const cb = (userAnswer) => {
-    const lives = calcLives(userAnswer, state.lives);
+    const lives = (userAnswer === Answer.WRONG) ? state.lives - 1 : state.lives;
     const answers = [...state.answers, userAnswer];
-    const gameOver = lives === 0 || answers.length === state.questions.length;
+    const gameOver = lives === 0 || answers.length === state.settings.questionNumber;
 
     const gameUpdate = {lives, answers, gameOver};
     callback(gameUpdate);
   };
 
   const newQuestion = state.questions[state.answers.length];
-  const newGame = Game[newQuestion.type](newQuestion, cb);
+  const game = renderTemplate(getGameTemplate(newQuestion.text));
+  const gameContent = Game[newQuestion.type](newQuestion, cb);
+  game.appendChild(gameContent);
+  return game;
+};
+
+export default (state, callback) => {
+  const screen = document.createElement(`template`);
+
+  const header = getHeader(state, callback);
+  const status = getStatus(state);
+  header.appendChild(status);
+
+  const game = getGame(state, callback);
 
   const stats = getStats(state.answers, state.questions.length);
   const statsContainer = renderTemplate(`<div class="stats"></div>`);
-
   statsContainer.appendChild(stats);
-  header.appendChild(status);
+
+  const footer = getFooter();
+
   screen.content.appendChild(header);
-  screen.content.appendChild(newGame);
+  screen.content.appendChild(game);
   screen.content.appendChild(statsContainer);
   screen.content.appendChild(footer);
-
   return screen.content;
 };
