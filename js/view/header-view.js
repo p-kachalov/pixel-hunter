07d8d6498127464e3171f6f8087daa2e5f4fd3a2
Pavel K <p.kachalov@yandex.ru>
2018-06-19 18:46:36 +0300

    add header view

---
+++ js/view/header-view.js
@@ -0,0 +1,28 @@
+import AbstractView from './abstract-view';
+
+class HeaderView extends AbstractView {
+  get template() {
+    return `
+    <header class="header">
+      <div class="header__back">
+        <button class="back">
+          <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
+          <img src="img/logo_small.svg" width="101" height="44">
+        </button>
+      </div>
+    </header>
+    `;
+  }
+
+  bind() {
+    const backButton = this.element.querySelector(`.back`);
+    backButton.addEventListener(`click`, () => {
+      this.callback();
+    });
+  }
+
+  callback() {
+  }
+}
+
+export default HeaderView;
