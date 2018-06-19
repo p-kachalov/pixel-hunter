d00903eeed5dd86db5749d69d20313164e623d78
Pavel K <p.kachalov@yandex.ru>
2018-06-20 01:36:44 +0300

    fix game view

--- js/view/game-view.js
+++ js/view/game-view.js
@@ -9,15 +9,6 @@ class GameView extends AbstractView {
     this._game = game;
   }
 
-  get template() {
-    return `
-    <div class="game">
-      <p class="game__task">${GameAttributes[this._gameType].question}</p>
-      <form class="game__content  ${GameAttributes[this._gameType].containerClass}"></form>
-    </div>
-    `;
-  }
-
   render() {
     const container = document.createElement(`template`);
     container.content.appendChild(this._header);
