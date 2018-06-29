39d67857f7832b617514f5d0522208636286d0c3
Pavel K <p.kachalov@yandex.ru>
2018-06-29 10:47:41 +0300

    add blink on timer

--- js/blocks/status-view.js
+++ js/blocks/status-view.js
@@ -8,7 +8,7 @@ class StatusView extends AbstractView {
 
   get template() {
     return `
-    <h1 class="game__timer">${this.state.time}</h1>
+    <h1 class="game__timer" ${this.state.time <= 5 ? `style="animation: blink-animation 1s steps(5, start) infinite"` : ``}>${this.state.time}</h1>
     <div class="game__lives">
       ${new Array(this.state.settings.maxLivesNumber - this.state.lives)
         .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`)
@@ -17,6 +17,13 @@ class StatusView extends AbstractView {
         .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
         .join(``)}
     </div>
+    <style>
+      @keyframes blink-animation {
+        to {
+          visibility: hidden;
+        }
+      }
+    </style>
     `;
   }
 }
