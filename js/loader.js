59a6297218d3054d956b69090d5d961c79982f77
Pavel K <p.kachalov@yandex.ru>
2018-07-02 20:50:08 +0300

    fix d13

--- js/loader.js
+++ js/loader.js
@@ -6,9 +6,9 @@ const APP_ID = `235268`;
 const checkStatus = (response) => {
   if (response.status >= 200 && response.status < 300) {
     return response;
-  } else {
-    throw new Error(`${response.status}: ${response.statusText}`);
   }
+
+  throw new Error(`${response.status}: ${response.statusText}`);
 };
 
 const toJSON = (res) => res.json();
